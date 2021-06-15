7982777b572cae1c0a3420415f23346e46a65da2
Zastrzyk <r.ogrodowczyk87@gmail.com>
2021-06-15 10:27:15 +0100

    Fade effect

--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 import '../index.css';
+import Fade from 'react-reveal/Fade';
 
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg}) {   
     return (
         <Wrap bgImage={backgroundImg}>
-            <ItemText>
-                <h1>{title}</h1>
-                <p>{description }</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description }</p>
+                </ItemText>
+            </Fade>
             <Buttons>
                 <ButtomGroup>
                     <LeftButtom>
