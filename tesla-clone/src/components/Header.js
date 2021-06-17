f6958ef92e939659f9258a2c1d75cba915440b48
Zastrzyk <r.ogrodowczyk87@gmail.com>
2021-06-17 12:02:46 +0100

    done website

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -2,19 +2,23 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from "../features/car/carSlice"
+import { useSelector } from 'react-redux'
 
 function Header() {
     const [burgerStatus, setBurgerStatus] = useState(false);
+    const cars = useSelector(selectCars)
+    
+
     return (
         <Container>
             <a>
                 <img src="/images/logo.svg"/>
             </a>
             <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
-                <a href="#">Model Y</a>
+                {cars && cars.map((car, index)=>(
+                    <a key={index} href="#">{car}</a>                   
+                ))}            
             </Menu>
             <RightMenu>
                 <a href="#">Shop</a>
@@ -25,12 +29,9 @@ function Header() {
                 <CloseWrapper>
                     <CustomClose onClick={() => setBurgerStatus(false)}/>
                 </CloseWrapper>
-                <li><a href="#">Existing Inventory</a></li>
-                <li><a href="#">Used Inventory</a></li>
-                <li><a href="#">Trade-in</a></li>
-                <li><a href="#">Cybertruck</a></li>
-                <li><a href="#">Roadster</a></li>
-                <li><a href="#">Semi</a></li>               
+                {cars && cars.map((car, index)=>(
+                    <li key={index} ><a href="#">{car}</a></li>                 
+                ))}                            
             </BurgerNav>
         </Container>
     )
@@ -75,6 +76,7 @@ const RightMenu = styled.div`
         font-weight: 600;
         text-transform: uppercase;
         pmargin-right: 10px;
+        margin-right: 10px;
     }
     `
 
@@ -95,6 +97,7 @@ const BurgerNav = styled.div`
     flex-direction: column;
     text-align: start;
     transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.2s ease-in-out;
     li {
         padding: 15px 0;
         border-bottom: 1px solid rgba(0, 0, 0, .2);
